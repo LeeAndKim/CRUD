1beeb03f3f684cd20951d15081e195b37763db90
leeheeweon <ggoddii@naver.com>
2022-04-03 17:14:54 +0900

    xiuxi yitian

--- whichSunday/src/main/java/com/yongin/whichSunday/common/interceptor/LogInterceptor.java
+++ whichSunday/src/main/java/com/yongin/whichSunday/common/interceptor/LogInterceptor.java
@@ -17,7 +17,6 @@ public class LogInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
         String uuid = UUID.randomUUID().toString();
-
         request.setAttribute(LOG_ID, uuid);
         log.info("REQUEST [{}][{}]", uuid, requestURI);
         return true;
